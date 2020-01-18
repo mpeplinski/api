b35acc02809426e73bec5a025fd2f730c2db503a
mpeplinski <60013848+mpeplinski@users.noreply.github.com>
2020-01-18 08:55:32 +0100

    wykres2

--- Main-to.py
+++ Main-to.py
@@ -66,3 +66,12 @@ plt.title('Osoby wykonujące pracę nierejestrowaną ogółem w roku 2017')
 plt.xlabel('Miesiące')
 plt.ylabel('Liczba oosób w Tysiącach')
 plt.show()
+
+
+ogolem_filtr2.plot(kind='bar')
+plt.title('Osoby wykonujące pracę nierejestrowaną jednorazowo w miesiącu')
+plt.xlabel('Miesiące')
+plt.ylabel('Liczba oosób w Tysiącach')
+plt.show()
+
+#data = ogolem_filtr1.groupby('razem total').mean()
